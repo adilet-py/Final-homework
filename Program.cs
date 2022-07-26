11df0a1d42daa9cae54f1d639317fad932f2687e
Adilet-py <z.skinny.fy@gmail.com>
2022-07-27 01:54:20 +0900

    Создание методов, составление блоксхемы

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
-﻿Console.WriteLine("Введите слова через запятую");
+﻿void methodArray (string [] array){
+     string[] newarr = new string[array.Length]
+     for (int i=0; i<array.Length; i++){      
+        if(array[i].Length<=3){
+        newarr[i]=array[i];;
+        Console.WriteLine(newarr[i]); 
+        }
+     }
+        Console.WriteLine(); 
+}
+
+void Print(string [] Array){
+    for (int i=0; i<Array.Length; i++){
+        Console.WriteLine(Array[i]);
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите слова через запятую");
 string s = Console.ReadLine();
 string[] str = s.Split(',');
 string[] result = new string[str.Length];
@@ -9,4 +27,9 @@ for (int i = 0; i < str.Length; i++)
     {
         result[i] = str[i];
     }
+}
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
 }
