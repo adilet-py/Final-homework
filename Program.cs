73c9be69d06e461115b1710db7cc305757bc540d
Adilet-py <z.skinny.fy@gmail.com>
2022-07-27 01:16:45 +0900

    Поиск слов, где <= 3 символа

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿Console.WriteLine("Введите слова через запятую");
 string s = Console.ReadLine();
 string[] str = s.Split(',');
+string[] result = new string[str.Length];
+
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i].Length <= 3)
+    {
+        result[i] = str[i];
+    }
+}
